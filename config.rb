0d2278c5e81c448b6ce49940b3f451672f7569c6
Patrick Arlt <patrick.arlt@gmail.com>
2014-05-06 15:05:00 -0700

    links

--- config.rb
+++ config.rb
@@ -6,10 +6,20 @@ set :images_dir, 'images'
 
 activate :directory_indexes
 
-activate :relative_assets
-
-set :relative_links, true
-
 activate :deploy do |deploy|
   deploy.method = :git
+end
+
+helpers do
+  def base_url
+    ''
+  end
+end
+
+configure :build do
+  helpers do
+    def base_url
+      '/write-the-docs-example'
+    end
+  end
 end
